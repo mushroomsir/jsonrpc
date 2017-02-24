7e02f13dd358c679e5751467042db2b752779971
pengwei <pengwei@teambition.com>
2017-02-24 17:25:04 +0800

    Support batch

--- jsonrpc.go
+++ jsonrpc.go
@@ -12,6 +12,16 @@ var (
 	request        = "request"
 	errorType      = "error"
 	success        = "success"
+	// RPCParseError means invalid JSON was received by the server.An error occurred on the server while parsing the JSON text.
+	RPCParseError = &ErrorObj{Code: -32700, Message: "Parse error"}
+	// RPCInvalidRequest means the JSON sent is not a valid Request object.
+	RPCInvalidRequest = &ErrorObj{Code: -32600, Message: "Invalid Request"}
+	// RPCNotFound means the method does not exist / is not available.
+	RPCNotFound = &ErrorObj{Code: -32601, Message: "Method not found"}
+	// RPCInvalidParams means Invalid method parameter(s).
+	RPCInvalidParams = &ErrorObj{Code: -32602, Message: "Invalid params"}
+	// RPCInternalError means Internal JSON-RPC error.
+	RPCInternalError = &ErrorObj{Code: -32603, Message: "Internal error"}
 )
 
 // ClientRequest represents a JSON-RPC data from client.
@@ -32,25 +42,25 @@ type PayloadReq struct {
 	ID interface{} `json:"id,omitempty"`
 }
 
-// ParseReq ...
-func ParseReq(msg string) (req *ClientRequest, err error) {
-	p := &PayloadReq{}
-	if err = validateMsg(msg, p); err != nil {
-		return
-	}
-	req = &ClientRequest{PlayLoad: p}
-	if p.Version != jsonRPCVersion {
-		req.Type = invalid
-		err = errors.New("invalid jsonrpc version")
-	} else if p.Method == "" {
-		req.Type = invalid
-		err = errors.New("invalid jsonrpc object")
-	} else if p.ID == nil {
-		req.Type = notification
-	} else {
-		req.Type = request
-	}
-	return
+// ClientResponse represents a JSON-RPC response returned to a client.
+type ClientResponse struct {
+	Type     string
+	PlayLoad *PayloadRes
+}
+
+// PayloadRes represents a JSON-RPC request.
+type PayloadRes struct {
+	Version string      `json:"jsonrpc"`
+	Result  interface{} `json:"result,omitempty"`
+	Error   *ErrorObj   `json:"error,omitempty"`
+	ID      interface{} `json:"id,omitempty"`
+}
+
+// ErrorObj ...
+type ErrorObj struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
 }
 
 // Request creates a JSON-RPC 2.0 request object, return JsonRpc json.
@@ -67,8 +77,8 @@ func Request(id interface{}, method string, args ...interface{}) (result string,
 	if len(args) > 0 {
 		p.Params = args[0]
 	}
-	b, err := json.Marshal(p)
-	return string(b), err
+	val, err := json.Marshal(p)
+	return string(val), err
 }
 
 // Notification Creates a JSON-RPC 2.0 notification object, return JsonRpc json.
@@ -76,37 +86,70 @@ func Notification(method string, args ...interface{}) (string, error) {
 	return Request(nil, method, args...)
 }
 
-// ClientResponse represents a JSON-RPC response returned to a client.
-type ClientResponse struct {
-	Type     string
-	PlayLoad *PayloadRes
+//Batch ...
+func Batch(batch ...string) (arrstr string) {
+	if len(batch) == 0 {
+		return "[]"
+	}
+	arrstr = "["
+	for index := 0; index < len(batch)-1; index++ {
+		arrstr += batch[index]
+		arrstr += ","
+	}
+	arrstr += batch[len(batch)-1]
+	arrstr += ",]"
+	return
 }
 
-// PayloadRes represents a JSON-RPC request.
-type PayloadRes struct {
-	Version string      `json:"jsonrpc"`
-	Result  interface{} `json:"result,omitempty"`
-	Error   *ErrorObj   `json:"error,omitempty"`
-	ID      interface{} `json:"id,omitempty"`
+// ParseReq ...
+func ParseReq(msg string) (req *ClientRequest, err error) {
+	p := make(map[string]interface{}, 0)
+	if err = validateMsg(msg, &p); err != nil {
+		return
+	}
+	req, err = parseReqMap(p)
+	return
+}
+
+// ParseReqBatch ...
+func ParseReqBatch(msg string) (req []*ClientRequest, err error) {
+	if msg == "" || len(msg) < 2 {
+		err = errors.New("empty message")
+		return
+	}
+	payloads := make([]map[string]interface{}, 0)
+	if err = validateMsg(msg, &payloads); err == nil {
+		req = make([]*ClientRequest, len(payloads))
+		for i, val := range payloads {
+			r, _ := parseReqMap(val)
+			req[i] = r
+		}
+	}
+	return
 }
 
 // ParseRes ...
 func ParseRes(msg string) (res *ClientResponse, err error) {
-
-	p := &PayloadRes{}
-	if err = validateMsg(msg, p); err != nil {
+	p := make(map[string]interface{}, 0)
+	if err = validateMsg(msg, &p); err != nil {
 		return
 	}
-	res = &ClientResponse{PlayLoad: p}
-	if p.Version != jsonRPCVersion {
-		res.Type = invalid
-		err = errors.New("invalid jsonrpc version")
-	} else if p.Error != nil {
-		res.Type = errorType
-	} else if p.Result != nil {
-		res.Type = success
-	} else {
-		err = errors.New("invalid jsonrpc object")
+	return parseResMap(p)
+}
+
+// ParseResBatch ...
+func ParseResBatch(msg string) (res []*ClientResponse, err error) {
+	if msg == "" || len(msg) < 2 {
+		err = errors.New("empty message")
+		return
+	}
+	payloads := make([]map[string]interface{}, 0)
+	if err = validateMsg(msg, &payloads); err == nil {
+		res = make([]*ClientResponse, len(payloads))
+		for i, val := range payloads {
+			r, _ := parseResMap(val)
+			res[i] = r
+		}
 	}
 	return
 }
@@ -126,13 +169,8 @@ func Success(id interface{}, result interface{}) (str string, err error) {
 		Result:  result,
 		ID:      id,
 	}
-	json, err := json.Marshal(p)
-	return string(json), err
-}
-
-// CreateError a JsonRpc error
-func CreateError(code int, msg string) *ErrorObj {
-	return &ErrorObj{Code: code, Message: msg}
+	data, err := json.Marshal(p)
+	return string(data), err
 }
 
 //Error Creates a JSON-RPC 2.0 error response object, return JsonRpc json.
@@ -145,16 +183,58 @@ func Error(id interface{}, rpcerr *ErrorObj) (str string, err error) {
 		Error:   rpcerr,
 		ID:      id,
 	}
-	json, err := json.Marshal(p)
-	return string(json), err
+	data, err := json.Marshal(p)
+	return string(data), err
 }
 
-// ErrorObj ...
-type ErrorObj struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+// CreateError a JsonRpc error
+func CreateError(code int, msg string, data ...interface{}) (obj *ErrorObj) {
+	obj = &ErrorObj{Code: code, Message: msg}
+	if len(data) > 0 {
+		obj.Data = data[0]
+	}
+	return obj
 }
 
+func parseResMap(val map[string]interface{}) (r *ClientResponse, err error) {
+	r = &ClientResponse{PlayLoad: &PayloadRes{}}
+	if version, ok := val["jsonrpc"]; ok {
+		r.PlayLoad.Version = version.(string)
+	}
+	if ID, ok := val["id"]; ok {
+		r.PlayLoad.ID = ID
+	}
+	if errs, ok := val["error"]; ok {
+		r.PlayLoad.Error = &ErrorObj{}
+		if err2, ok := errs.(map[string]interface{}); ok {
+			r.PlayLoad.Error.Code = int(err2["code"].(float64))
+			r.PlayLoad.Error.Message = err2["message"].(string)
+			r.PlayLoad.Error.Data = err2["data"]
+		}
+	}
+	if result, ok := val["result"]; ok {
+		r.PlayLoad.Result = result
+	}
+	err = checkResType(r)
+	return
+}
+func parseReqMap(val map[string]interface{}) (r *ClientRequest, err error) {
+	r = &ClientRequest{PlayLoad: &PayloadReq{}}
+	if version, ok := val["jsonrpc"]; ok {
+		r.PlayLoad.Version = version.(string)
+	}
+	if ID, ok := val["id"]; ok {
+		r.PlayLoad.ID = ID
+	}
+	if method, ok := val["method"]; ok {
+		r.PlayLoad.Method = method.(string)
+	}
+	if params, ok := val["params"]; ok {
+		r.PlayLoad.Params = params
+	}
+	err = checkReqType(r)
+	return
+}
 func validateID(id interface{}) (err error) {
 	if id != nil {
 		switch id.(type) {
@@ -174,7 +254,35 @@ func validateMsg(msg string, p interface{}) (err error) {
 	err = json.Unmarshal([]byte(msg), p)
 	if err != nil {
 		err = errors.New("invalid jsonrpc message structures")
-		return
+	}
+	return
+}
+func checkResType(res *ClientResponse) (err error) {
+	p := res.PlayLoad
+	if p.Version != jsonRPCVersion {
+		res.Type = invalid
+		err = errors.New("invalid jsonrpc version")
+	} else if p.Error != nil {
+		res.Type = errorType
+	} else if p.Result != nil {
+		res.Type = success
+	} else {
+		err = errors.New("invalid jsonrpc object")
+	}
+	return
+}
+func checkReqType(res *ClientRequest) (err error) {
+	p := res.PlayLoad
+	if p.Version != jsonRPCVersion {
+		res.Type = invalid
+		err = errors.New("invalid jsonrpc version")
+	} else if p.Method == "" {
+		res.Type = invalid
+		err = errors.New("invalid jsonrpc object")
+	} else if p.ID == nil {
+		res.Type = notification
+	} else {
+		res.Type = request
 	}
 	return
 }
--- jsonrpc_test.go
+++ jsonrpc_test.go
@@ -35,6 +35,24 @@ func TestProducer(t *testing.T) {
 		assert.Equal("{\"jsonrpc\":\"2.0\",\"method\":\"update\",\"params\":0}", val)
 
 	})
+	t.Run("jsonrpc with Batch func that should be", func(t *testing.T) {
+		assert := assert.New(t)
+
+		jsonrpc.Request("1", "sum")
+
+		val, err := jsonrpc.Request(123, "update")
+		assert.NotNil(err)
+		assert.Equal("{\"jsonrpc\":\"2.0\",\"method\":\"update\",\"id\":123}", val)
+
+		val2, err := jsonrpc.Request("123", "update")
+		assert.Equal("{\"jsonrpc\":\"2.0\",\"method\":\"update\",\"id\":\"123\"}", val)
+
+		val = jsonrpc.Batch(val, val2)
+		assert.Equal("[{\"jsonrpc\":\"2.0\",\"method\":\"update\",\"id\":\"123\"},{\"jsonrpc\":\"2.0\",\"method\":\"update\",\"id\":123}]", val)
+
+		val = jsonrpc.Batch()
+		assert.Equal("[]", val)
+	})
 	t.Run("jsonrpc with response that should be", func(t *testing.T) {
 		assert := assert.New(t)
 
@@ -50,12 +68,24 @@ func TestProducer(t *testing.T) {
 		assert.Nil(err)
 		assert.Equal("{\"jsonrpc\":\"2.0\",\"result\":[],\"id\":123}", val)
 
+		val, err = jsonrpc.Success(true, "")
+		assert.NotNil(err)
+		assert.Equal("invalid id that MUST contain a String, Number, or NULL value", err.Error())
+
 		rpcerr := jsonrpc.CreateError(1, "test")
 		val, err = jsonrpc.Error(nil, rpcerr)
 		assert.Equal("{\"jsonrpc\":\"2.0\",\"error\":{\"code\":1,\"message\":\"test\"}}", val)
+
+		val, err = jsonrpc.Error(true, rpcerr)
+		assert.NotNil(err)
+		assert.Equal("invalid id that MUST contain a String, Number, or NULL value", err.Error())
+
+		rpcerr = jsonrpc.CreateError(1, "test", "xx")
+		val, err = jsonrpc.Error(nil, rpcerr)
+		assert.Equal("{\"jsonrpc\":\"2.0\",\"error\":{\"code\":1,\"message\":\"test\",data:\"xx\"}}", val)
 	})
 
-	t.Run("jsonrpc with parsereq that should be", func(t *testing.T) {
+	t.Run("jsonrpc with ParseReq func that should be", func(t *testing.T) {
 
 		assert := assert.New(t)
 
@@ -89,7 +119,7 @@ func TestProducer(t *testing.T) {
 		assert.Equal("invalid jsonrpc object", err.Error())
 
 	})
-	t.Run("jsonrpc with parseres that should be", func(t *testing.T) {
+	t.Run("jsonrpc with ParseRes fun that should be", func(t *testing.T) {
 		assert := assert.New(t)
 
 		val, err := jsonrpc.ParseRes("")
@@ -118,9 +148,80 @@ func TestProducer(t *testing.T) {
 		assert.Equal(errorType, val.Type)
 
 		val, err = jsonrpc.ParseRes("{\"jsonrpc\":\"2.0\",\"id\":\"123\"}")
-		assert.NotNil(err)
-		assert.Equal("invalid jsonrpc object", err.Error())
+		if assert.NotNil(err) {
+			assert.Equal("invalid jsonrpc object", err.Error())
+		}
 
+		val, err = jsonrpc.ParseRes("{\"jsonrpc\": \"2.0\", \"error\": {\"code\": -32601, \"message\": \"Method not found\"}, \"id\": \"1\"}")
+		assert.Nil(err)
+		assert.Equal("1", val.PlayLoad.ID)
+		assert.Equal(-32601, val.PlayLoad.Error.Code)
+		assert.Equal(errorType, val.Type)
+
+	})
+	t.Run("jsonrpc with ParseResBatch func that should be", func(t *testing.T) {
+		arr := "[{\"jsonrpc\": \"2.0\", \"error\": {\"code\": -32601, \"message\": \"Method not found\"}, \"id\": null},{\"jsonrpc\": \"2.0\", \"error\": {\"code\": -32601, \"message\": \"Method not found\"}, \"id\": \"1\"},{\"jsonrpc\": \"2.0\", \"error\": {\"code\": -32601, \"message\": \"Method not found\"}, \"id\": \"2\"}]"
+		assert := assert.New(t)
+
+		val, err := jsonrpc.ParseResBatch(arr)
+
+		if assert.Nil(err) {
+			assert.Equal(3, len(val))
+			assert.Equal(nil, val[0].PlayLoad.ID)
+			assert.Equal(-32601, val[0].PlayLoad.Error.Code)
+			assert.Equal("Method not found", val[0].PlayLoad.Error.Message)
+			assert.Equal("1", val[1].PlayLoad.ID)
+			assert.Equal(-32601, val[1].PlayLoad.Error.Code)
+			assert.Equal("Method not found", val[1].PlayLoad.Error.Message)
+			assert.Equal("2", val[2].PlayLoad.ID)
+			assert.Equal(-32601, val[2].PlayLoad.Error.Code)
+			assert.Equal("Method not found", val[2].PlayLoad.Error.Message)
+		}
+
+		val, err = jsonrpc.ParseResBatch("")
+		assert.Equal("empty message", err.Error())
+
+		str := `[
+        {"jsonrpc": "2.0", "result": 7, "id": "1"},
+        {"jsonrpc": "2.0", "result": 19, "id": "2"},
+        {"jsonrpc": "2.0", "error": {"code": -32600, "message": "Invalid Request"}, "id": null},
+        {"jsonrpc": "2.0", "error": {"code": -32601, "message": "Method not found"}, "id": 5},
+        {"jsonrpc": "2.0", "result": ["hello", 5], "id": "9"}
+      ]`
+
+		val, err = jsonrpc.ParseResBatch(str)
+		assert.Equal("1", val[0].PlayLoad.ID)
+		assert.Equal(float64(19), val[1].PlayLoad.Result)
+		assert.Equal("Invalid Request", val[2].PlayLoad.Error.Message)
+		assert.Equal(float64(5), val[3].PlayLoad.ID)
+		assert.Equal([]interface{}{"hello", float64(5)}, val[4].PlayLoad.Result.([]interface{}))
+
+	})
+	t.Run("jsonrpc with ParseReqBatch func that should be", func(t *testing.T) {
+		arr := `[
+			{"jsonrpc": "2.0", "method": "sum", "params": [1,2,4], "id": "1"},
+			{"jsonrpc": "2.0", "method": "notify_hello", "params": [7]},
+			{"jsonrpc": "2.0", "method": "subtract", "params": [42,23], "id": "2"},
+			{"foo": "boo"},
+			{"jsonrpc": "2.0", "method": "foo.get", "params": {"name": "myself"}, "id": "5"},
+			{"jsonrpc": "1.0", "method": "get_data", "id": "9"} 
+    	]`
+		assert := assert.New(t)
+
+		val, err := jsonrpc.ParseReqBatch(arr)
+
+		assert.Nil(err)
+
+		if assert.Equal(6, len(val)) {
+			assert.Equal("1", val[0].PlayLoad.ID)
+			assert.Equal("notify_hello", val[1].PlayLoad.Method)
+			assert.Equal([]interface{}{float64(42), float64(23)}, val[2].PlayLoad.Params.([]interface{}))
+			assert.Equal(invalid, val[3].Type)
+			assert.Equal("myself", val[4].PlayLoad.Params.(map[string]interface{})["name"])
+			assert.Equal(invalid, val[5].Type)
+		}
+		val, err = jsonrpc.ParseReqBatch("")
+		assert.Equal("empty message", err.Error())
 	})
 
 }
